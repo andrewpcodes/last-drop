8f8b399ecdddd7aefe88d760883653a577e1d56a
Andrew Peterson <apeterson247@gmail.com>
2024-12-05 22:31:34 -0500

    Feature: new recipe endpoints (#10)
    * Update with new recipe endpoints
    
    * Add Endpoints
    
    * Update README with endpoint documentation

--- src/routes/recipe.routes.ts
+++ src/routes/recipe.routes.ts
@@ -1,23 +1,59 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import Recipe from '../models/Recipe';
 
 const router = Router();
 
-// Create a new recipe
-router.post('/', async (req, res) => {
+// Get all recipes
+router.get('/', async (req: Request, res: Response) => {
   try {
-    const recipe = new Recipe(req.body);
-    await recipe.save();
-    res.status(201).send(recipe);
+    const recipes = await Recipe.find();
+    res.status(200).send(recipes);
   } catch (error) {
-    res.status(400).send(error);
+    res.status(500).send(error);
   }
 });
 
-// Get all recipes
-router.get('/', async (req, res) => {
+// Get a recipe by ID
+router.get('/recipe/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
   try {
-    const recipes = await Recipe.find();
+    const recipe = await Recipe.findById(id);
+    if (!recipe) {
+      res.status(404).send('Recipe not found');
+    }
+    res.status(200).send(recipe);
+  } catch (error) {
+    res.status(500);
+  }
+});
+
+// POST get recipes by name
+router.post('/name', async (req: Request, res: Response) => {
+  const { name } = req.body;
+  try {
+    const recipes = await Recipe.find({ name: { $regex: new RegExp(name, 'i') } });
+    res.status(200).send(recipes);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// POST get recipes by category(s)
+router.post('/categories', async (req: Request, res: Response) => {
+  const { categories } = req.body;
+  try {
+    const recipes = await Recipe.find({ categories: { $in: categories } });
+    res.status(200).send(recipes);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// POST get recipes by ingredient(s)
+router.post('/ingredients', async (req: Request, res: Response) => {
+  const { ingredients } = req.body;
+  try {
+    const recipes = await Recipe.find({ ingredients: { $elemMatch: { ingredient: { $in: ingredients } } } });
     res.status(200).send(recipes);
   } catch (error) {
     res.status(500).send(error);
